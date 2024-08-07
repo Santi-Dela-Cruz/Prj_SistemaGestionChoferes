f382208ce7f948d44d9f04d7d28666eb9d4e9075
Ivan-dela-Cruz <canarivan495@gmail.com>
2024-08-07 16:14:07 -0500

    Ultimo cambio

--- Prj_SistemaGestionChoferes/src/App.java
+++ Prj_SistemaGestionChoferes/src/App.java
@@ -8,11 +8,11 @@ import ui.gui.MainPanelController;
 public class App {
     public static void main(String[] args) throws Exception {
         // LimpiaTablas limpiaTablas = new LimpiaTablas();
-         LoginAdministradorForm loginForm = new LoginAdministradorForm();
+        LoginAdministradorForm loginForm = new LoginAdministradorForm();
         loginForm.setVisible(true);
 
-       // MainPanelController mainPanel = new MainPanelController();
-        //mainPanel.setVisible(true);
+        // MainPanelController mainPanel = new MainPanelController();
+        // mainPanel.setVisible(true);
 
         // limpiaTablas.limpiarTodasTablas();
 
